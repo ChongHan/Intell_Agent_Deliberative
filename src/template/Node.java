64e00e42eb650a5eb90136ad5d0557055b1ea708
samsara <chong.han@epfl.ch>
2015-10-26 17:53:07 +0100

    Equals&hashcode

--- src/template/Node.java
+++ src/template/Node.java
@@ -2,55 +2,93 @@ package template;
 
 import logist.plan.Plan;
 
-import java.util.List;
-
 /**
- * This Class represent a node in our search tree with all the related information including the update plan from
- * the root of the tree.
- * Created by Daniel on 23.10.15.
+ * This Class represent a node in our search tree with all the related information including the update plan from the
+ * root of the tree. Created by Daniel on 23.10.15.
  */
-public class Node {
+public class Node
+{
 
+    private static double capacity;
     private State currentState = null;
     private Plan plan = null;
-    private static double capacity;
     private double load = 0;
     private double cost = 0; //f function f = g + h
 
-    public Node (State newState, Plan parentPlan, double newLoad, double newCapacity){
+    public Node(State newState, Plan parentPlan, double newLoad, double newCapacity)
+    {
         plan = parentPlan;
         currentState = newState;
         capacity = newCapacity;
         load = newLoad;
     }
 
-    public Node (State newState, Plan parentPlan, double newLoad){
+    public Node(State newState, Plan parentPlan, double newLoad)
+    {
         plan = parentPlan;
         currentState = newState;
         load = newLoad;
     }
 
-    public Node (State newState, Plan parentPlan){
+    public Node(State newState, Plan parentPlan)
+    {
+
         plan = parentPlan;
         currentState = newState;
         load = 0;
     }
 
-    private int costFunction(){
+    public static double getCapacity()
+    {
+        return capacity;
+    }
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Node node = (Node) o;
+
+        return currentState.equals(node.currentState);
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return currentState.hashCode();
+    }
+
+    private int costFunction()
+    {
         //TODO
         return 0;
     }
 
-    public State getCurrentState() {return currentState;}
+    public State getCurrentState()
+    {
+        return currentState;
+    }
 
-    public Plan getPlan() {return plan;}
+    public Plan getPlan()
+    {
+        return plan;
+    }
 
-    public double getCost() {return cost;}
+    public double getCost()
+    {
+        return cost;
+    }
 
-    public double getLoad() {return load;}
+    public double getLoad()
+    {
+        return load;
+    }
 
-    public static double getCapacity() {return capacity;}
-
-    public double getFreeSpace() {return (capacity-load);}
+    public double getFreeSpace()
+    {
+        return (capacity - load);
+    }
 
 }
--- src/template/State.java
+++ src/template/State.java
@@ -1,32 +1,55 @@
 package template;
 
 import logist.task.Task;
-import logist.task.TaskSet;
 import logist.topology.Topology.City;
 
 import java.util.Hashtable;
-import java.util.List;
 
 /**
- * This class represent the State of our simulation at a given time.
- * Created by Daniel on 23.10.15.
+ * This class represent the State of our simulation at a given time. Created by Daniel on 23.10.15.
  */
-public class State {
+public class State
+{
 
 
     private City agentPosition;
-    private Hashtable<Task,City> tasksPosition;
+    private Hashtable<Task, City> tasksPosition;
 
-    State (City position, Hashtable<Task,City> tasksTable){
+    State(City position, Hashtable<Task, City> tasksTable)
+    {
         agentPosition = position;
+
         tasksPosition = tasksTable;
     }
 
-    public City getAgentPosition() {
+    @Override
+    public boolean equals(Object o)
+    {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        State state = (State) o;
+
+        if (!agentPosition.equals(state.agentPosition)) return false;
+        return tasksPosition.equals(state.tasksPosition);
+
+    }
+
+    @Override
+    public int hashCode()
+    {
+        int result = agentPosition.hashCode();
+        result = 31 * result + tasksPosition.hashCode();
+        return result;
+    }
+
+    public City getAgentPosition()
+    {
         return agentPosition;
     }
 
-    public Hashtable getTasksPosition() {
+    public Hashtable getTasksPosition()
+    {
         return tasksPosition;
     }
 }
